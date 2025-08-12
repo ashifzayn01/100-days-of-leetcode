9b6bf59f00f5aa25373969c0cad1fb66bfe1cca2
Md Ashif <kgnembroidery6@gmail.com>
2025-08-12 23:47:24 +0530

    kids with the maximum candies problem solved

---
+++ Day-1/problem4.java
@@ -0,0 +1,22 @@
+//Kids With the Greatest Number of Candies
+
+
+
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> result = new ArrayList<>();
+        int max = 0;
+        for(int candy : candies){
+            if (candy > max)
+            max  = candy;
+        }
+        for(int candy:candies){
+            if(candy + extraCandies >= max){
+                result.add(true);
+            }else{
+                result.add(false);
+            }
+        }
+        return result;
+    }
+}
